edbbec2dd4f0f76ff119dc14f80e83a81d291b81
Ekaterina <ushakova.esu@gmail.com>
2023-10-10 23:54:33 +0600

    main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -7,7 +7,7 @@ export const Main = () => {
                 <span>Hi there! </span>
                 <h2>My name is </h2>
                 <h2>Ekaterina Ushakova. </h2>
-                <h1>I am frontend-developer </h1>
+                <h1>I am frontend-developer. </h1>
             </div>
         </div>
     );
